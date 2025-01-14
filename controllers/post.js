5b75764785fe7699cb31839f8823ce8ed7efb8ac
MrAMIR8H0OSEIN <amirhosein44044881@gmail.com>
2025-01-14 16:21:01 +0330

    controllers

---
+++ controllers/post.js
@@ -0,0 +1,33 @@
+const Post = require('../models/post');
+
+exports.getPosts = (req,res)=>{
+    res.status(200).json({
+      posts: [
+        {
+          title: "test",
+          content: "test",
+        },
+      ],
+    });
+}
+
+exports.addPost = async(req,res)=>{
+    const title = req.body.title;
+    const content = req.body.content;
+    
+    const newPost = Post({
+      title: title,
+      content: content,
+      imageUrl: "images/sample.png",
+      creator: {
+        name: "Amirhosein Masalegooha",
+      },
+    });
+
+    const result = await newPost.save();
+
+    res.stauts(200).json({
+        message: "Successfully",
+        post: result
+    })
+}
