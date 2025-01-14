8a1dc0cb346ee664fc03a4674971528ae444effc
MrAMIR8H0OSEIN <amirhosein44044881@gmail.com>
2025-01-14 16:44:26 +0330

    validation

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const { validationResult } = require('express-validator');
 
 exports.getPosts = (req,res)=>{
     res.status(200).json({
@@ -12,22 +13,31 @@ exports.getPosts = (req,res)=>{
 }
 
 exports.addPost = async(req,res)=>{
-    const title = req.body.title;
-    const content = req.body.content;
-    
-    const newPost = Post({
-      title: title,
-      content: content,
-      imageUrl: "images/sample.png",
-      creator: {
-        name: "Amirhosein Masalegooha",
-      },
-    });
-
-    const result = await newPost.save();
-
-    res.status(200).json({
-        message: "Successfully",
-        post: result
+  const validate = validationResult(req);
+  if(!validate.isEmpty()){
+    res.status(202).json({
+      message: "validation faild",
+      errors: validate.array()
     })
+    return;
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+  
+  const newPost = Post({
+    title: title,
+    content: content,
+    imageUrl: "images/sample.png",
+    creator: {
+      name: "Amirhosein Masalegooha",
+    },
+  });
+
+  const result = await newPost.save();
+
+  res.status(200).json({
+      message: "Successfully",
+      post: result
+  })
 }
--- routes/default.js
+++ routes/default.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const {
+    body
+} = require('express-validator');
 
 const router = express.Router();
 
 const posts = require('../controllers/post');
-router.get('/posts',posts.getPosts);
-router.post('/posts',posts.addPost);
+router.get('/posts', posts.getPosts);
+router.post('/posts', [
+    body(['title', 'content'])
+    .isString().trim().withMessage('مقدار ورودی حتما باید یک رشته باشد')
+    .isLength({min: 5}).withMessage('مقدار ورودی حداقل باید 5 کاراکتر داشته باشد'),
+], posts.addPost);
 
 module.exports = router;
